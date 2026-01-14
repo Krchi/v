df46962d22c6cb50d18e658644a72b2d90689cef
Krchi <997054144@qq.com>
2026-01-14 09:02:19 +0800

    -

--- vlib/v/transformer/array.v
+++ vlib/v/transformer/array.v
@@ -18,8 +18,9 @@ pub fn (mut t Transformer) array_init(mut node ast.ArrayInit) ast.Expr {
 	if node.has_init {
 		node.init_expr = t.expr(mut node.init_expr)
 	}
-	if t.pref.backend == .js_node || node.elem_type.has_flag(.generic) || t.skip_array_transform
-		|| node.is_fixed || t.inside_in || node.has_len || node.has_cap || node.exprs.len == 0 {
+	if t.pref.backend !in [.c, .native, .wasm] || node.elem_type.has_flag(.generic)
+		|| t.skip_array_transform || node.is_fixed || t.inside_in || node.has_len || node.has_cap
+		|| node.exprs.len == 0 {
 		return node
 	}
 	// For C and native transform into a function call `builtin__new_array_from_c_array_noscan(...)` etc
